3b9954ec40bc5f01758edcfade62f0a5dc29f419
Marc Sallin <marc.sallin@outlook.com>
2019-03-07 15:19:40 +0100

    Add functionality to group commits by time interval

---
+++ src/Extraction/GitDataExtractor/GitDataExtractor/Aggregator/IntervalGroupedCommitCollection.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using GitDataExtractor.Model;
+
+namespace GitDataExtractor.Aggregator
+{
+    internal class IntervalGroupedCommitCollection : LinkedList<IntervalCommitGroup>
+    {
+        private readonly DateTime _startTime;
+        private readonly TimeSpan _interval;
+        private readonly IEnumerable<Commit> _history;
+
+        public IntervalGroupedCommitCollection(DateTime startTime, TimeSpan interval, IEnumerable<Commit> history)
+        {
+            _startTime = startTime;
+            _interval = interval;
+            _history = history;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            AddLast(new IntervalCommitGroup());
+
+            DateTime takeUntil = _startTime + _interval;
+
+            var commitQueue = new Queue<Commit>(_history);
+            while (commitQueue.TryDequeue(out Commit commit))
+            {
+                // The current commit isn't in the current time range anymore.
+                // As the commits are ordered, a new bag must be started.
+                if (commit.Date > takeUntil)
+                {
+                    AddLast(new IntervalCommitGroup());
+                    Last.Value.StartTime = takeUntil;
+                    takeUntil += _interval;
+                    Last.Value.EndTime = takeUntil;
+                }
+
+                Last.Value.AddLast(commit);
+            }
+        }
+    }
+}
