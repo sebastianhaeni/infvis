e756085d273eaf1ffea4b4e5fe4b4a4abb21c96e
Marc Sallin <marc.sallin@outlook.com>
2019-03-27 14:11:55 +0100

    Exclude unnecessary properties from aggregate json serialization

--- src/Extraction/GitDataExtractor/GitDataExtractor/Models/Aggregation/File.cs
+++ src/Extraction/GitDataExtractor/GitDataExtractor/Models/Aggregation/File.cs
@@ -1,17 +1,22 @@
+﻿using Newtonsoft.Json;
+
 namespace GitDataExtractor.Models.Aggregation
 {
     public class File
     {
         public string FilePath { get; set; }
 
+        [JsonIgnore]
         public int AbsoluteLinesDelta => LinesAdded + LinesDeleted;
 
         public int RelativeLinesDelta => LinesAdded - LinesDeleted;
 
         public int LinesOfCode { get; set; }
 
+        [JsonIgnore]
         public int LinesAdded { get; set; }
 
+        [JsonIgnore]
         public int LinesDeleted { get; set; }
     }
 }
